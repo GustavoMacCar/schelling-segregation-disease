d6237b928e31e034d03665aed9f422a4319bce1d
GustavoMacCar <gmcmacar@gmail.com>
2022-08-25 22:30:01 -0300

    Squashed 'labs/GustavoMacCar/schelling-segregation-disease/' content from commit 20e89c8
    git-subtree-dir: labs/GustavoMacCar/schelling-segregation-disease
    git-subtree-split: 20e89c8db95b5b01b64554e1a748a280d6c50b94

---
+++ schelling/server.py
@@ -0,0 +1,71 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
+from traceback2 import print_tb
+
+from model import Schelling
+
+
+class HappyElement(TextElement):
+    """
+    Display a text count of how many sick agents there are.
+    """
+
+    def __init__(self):
+        pass
+
+    def render(self, model):
+        return "Happy agents: " + str(model.happy)
+
+
+class SickElement(TextElement):
+    """
+    Display a text count of how many sick agents there are.
+    """
+
+    def __init__(self):
+        pass
+
+    def render(self, model):
+        return "Sick agents: " + str(model.sick_count)
+
+
+
+def schelling_draw(agent):
+    """
+    Portrayal Method for canvas
+    """
+    if agent is None:
+        return
+    portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
+
+    if agent.type == 0:
+        portrayal["Color"] = ["#FF0000", "#FF9999"]
+        portrayal["stroke_color"] = "#00FF00"
+    else:
+        portrayal["Color"] = ["#0000FF", "#9999FF"]
+        portrayal["stroke_color"] = "#000000"
+    return portrayal
+
+
+happy_element = HappyElement()
+sick_element = SickElement()
+canvas_element = CanvasGrid(schelling_draw, 20, 20, 500, 500)
+happy_chart = ChartModule([{"Label": "happy", "Color": "Black"}, {"Label": "sick", "Color": "Red"}])
+
+
+model_params = {
+    "height": 20,
+    "width": 20,
+    "density": UserSettableParameter("slider", "Agent density", 0.8, 0.1, 1.0, 0.1),
+    "minority_pc": UserSettableParameter(
+        "slider", "Fraction minority", 0.2, 0.00, 1.0, 0.05
+    ),
+    "homophily": UserSettableParameter("slider", "Homophily", 3, 0, 8, 1),
+    "disease_probability": UserSettableParameter("slider", "Disease probability", 50, 0, 100, 1),
+    "spread_probability": UserSettableParameter("slider", "Spread probability", 50, 0, 100, 1),
+}
+
+server = ModularServer(
+    Schelling, [canvas_element, happy_element, sick_element, happy_chart], "Schelling", model_params
+)
